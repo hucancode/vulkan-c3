c337a3f38b365ef337f4e554ab3528f43db3c1c4
USER <hucancode@gmail.com>
2025-02-24 15:32:39 +0900

    use c3c on git

--- flake.nix
+++ flake.nix
@@ -1,28 +1,32 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    c3c.url = "github:c3lang/c3c?tag=0.6.7";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, c3c }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
+        c3cPkg = c3c.packages.${system}.default or null;
       });
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({ pkgs, c3cPkg }: {
         default = pkgs.mkShell.override
           { }
           {
             packages = with pkgs; [
-              # c3c
               glfw
               freetype
               vulkan-loader
               gnumake
               shaderc
               tracy
-            ] ++ (if system == "x86_64-darwin" || system == "aarch64-darwin" then [ moltenvk ] else [ vulkan-validation-layers ]);
+            ] ++ (if system == "x86_64-darwin" || system == "aarch64-darwin" then [ moltenvk ] else [ vulkan-validation-layers ])
+            ++ (if c3cPkg != null then [ c3cPkg ] else [ ]);
           };
       });
     };
